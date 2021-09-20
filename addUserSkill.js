5c0959a2a5f68689d73505e3ac6490e26ca02ce0
abhilashsthakur <abhilashsinghthakur@gmail.com>
2021-09-20 17:27:01 +0530

    2c

--- addUserSkill.js
+++ addUserSkill.js
@@ -1,62 +1,58 @@
-var users = [
-	{
-		name:'Brook', 
-		scores:75,
-		skills:['HTM', 'CSS', 'JS'],
-		age:16
-	},
-	{
-		name:'Alex', 
-		scores:80,
-		skills:['HTM', 'CSS', 'JS'],
-		age:18
-	}, 
-	{
-		name:'David', 
-		scores:75,
-		skills:['HTM', 'CSS'],
-		age:22
-	}, 
-	{
-		name:'John', 
-		scores:85,
-		skills:['HTM'],
-		age:25
-	},
-	{
-		name:'Sara',
-		scores:95,
-		skills:['HTM', 'CSS', 'JS'],
-		age: 26
-	},
-	{
-		name:'Martha', 
-		scores:80,
-		skills:['HTM', 'CSS', 'JS'],
-		age:18
-	},
-	{
-		name:'Thomas',
-		scores:90,
-		skills:['HTM', 'CSS', 'JS'],
-		age:20
-	}
-	]
+const users = [
+    {
+        name: 'Brook',
+        scores: 75,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 16
+    },
+    {
+        name: 'Alex',
+        scores: 80,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 18
+    },
+    {
+        name: 'David',
+        scores: 75,
+        skills: ['HTM', 'CSS'],
+        age: 22
+    },
+    {
+        name: 'John',
+        scores: 85,
+        skills: ['HTM'],
+        age: 25
+    },
+    {
+        name: 'Sara',
+        scores: 95,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 26
+    },
+    {
+        name: 'Martha',
+        scores: 80,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 18
+    },
+    {
+        name: 'Thomas',
+        scores: 90,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 20
+    }
+];
 
-	function updateUser(users,str,newSkill){
+function addUserSkill(users, userName, newSkills) {
 
-		for(let i=0;i<users.length;i++){
-		
-			if(users[i].name===str){
-		
-				let newUpdate=Object.assign({},users[i],{skills:[...['HTM', 'CSS', 'JS'], newSkill]})
-		
-				console.log(users[i])
-		
-				console.log("")
-		
-				console.log(newUpdate)
-			}
-		}
-	}
-	updateUser(users,'Thomas','cde')
+    const index = users.findIndex(value => value.name.toLowerCase() === userName.toLowerCase())
+
+    //    {} to make a copy of object to be modified
+
+    const newValue = Object.assign({}, users[index], { skills: users[index].skills.concat(newSkills) })
+
+    return [users[index], newValue]
+
+}
+
+console.log(addUserSkill(users, 'John', 'abcd'))
